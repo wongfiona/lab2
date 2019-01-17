415bcde3988fdee178a4ad7e9cdfefa8ca58915a
Fiona Wong <wong.fiona.11@gmail.com>
2019-01-17 10:44:34 -0800

    edited js

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -1,3 +1,11 @@
+$(document).ready(function() {
+	initializePage();
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+
 function initializePage() {
   $("#testjs").click(function(e) {
     $('.jumbotron h1').text("JavaScript is connected");
